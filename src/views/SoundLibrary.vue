6f19be389514feb50a888da789405cb3cfdd10e8
Marc Calder <marc.calder@gmail.com>
2018-12-29 20:56:52 -0500

    Building the Sound Upload facility.

--- src/views/SoundLibrary.vue
+++ src/views/SoundLibrary.vue
@@ -14,6 +14,12 @@ export default {
       lstCategories: [],
       lstSounds: [],
       activeCategory: 0,
+      dialogVisible: false,
+      uploadForm: {
+        name: '',
+        description: '',
+        categories: [],
+      },
     }
   },
   watch: {
@@ -36,6 +42,10 @@ export default {
       } catch (e) {
         this.errors.push(e)
       }
+    },
+    submitUpload() {
+      this.$refs.upload.action = `${settings.orchestrationAPI}\\sounds`
+      this.$refs.upload.submit()
     },
   },
   async mounted() {
@@ -61,8 +71,18 @@ export default {
   <div>
     <h1>Sound Library</h1>
     <div id="toolbar" class="flex-container">
-      <div class="flex-item"><el-button type="primary" size="small">Search&nbsp;<i class="el-icon-search el-icon-right"></i></el-button></div>&nbsp;&nbsp;
-      <div class="flex-item"><el-button type="primary" size="small">Upload&nbsp;<i class="el-icon-upload el-icon-right"></i></el-button></div>
+      <div class="flex-item">
+        <el-button type="primary" size="small">
+          Search&nbsp;
+          <i class="el-icon-search el-icon-right"></i>
+        </el-button>
+      </div>&nbsp;&nbsp;
+      <div class="flex-item">
+        <el-button type="primary" size="small" @click="dialogVisible = true">
+          Upload&nbsp;
+          <i class="el-icon-upload el-icon-right"></i>
+        </el-button>
+      </div>
     </div>
 
     <div>
@@ -87,42 +107,77 @@ export default {
         </el-collapse-item>
       </el-collapse>
     </div>
+    <el-dialog title="Upload a New Sound" :visible.sync="dialogVisible" width="40%">
+      <span>You can upload your own sound files to Airhorn. Sounds must be PCM Waveform files (.wav)</span>
+      <br>
+      <el-form :model="uploadForm" label-width="80px">
+        <el-form-item label="Title:">
+          <el-input type="text" :model="uploadForm.name" placeholder="The title of your sound."></el-input>
+        </el-form-item>
+        <el-form-item label="Description:">
+          <el-input type="text" :model="uploadForm.description" placeholder="A short description"></el-input>
+        </el-form-item>
+        <el-form-item label="Categories:">
+          <el-select v-model="uploadForm.categories" multiple placeholder="Select">
+            <el-option
+              v-for="category in lstCategories"
+              :key="category.id"
+              :label="category.name"
+              :value="category.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-upload ref="upload" action :auto-upload="false" accept=".wav">
+          <el-button slot="trigger" size="small" type="primary">select file</el-button>
+        </el-upload>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button size="small" @click="dialogVisible = false">Cancel</el-button>
+        <el-button
+          style="margin-left: 10px;"
+          size="small"
+          type="success"
+          @click="submitUpload"
+        >Upload to Airhorn Unit</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <style scoped>
 .flex-container {
-    display: -ms-flexbox;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-flex-direction: row;
-    -ms-flex-direction: row;
-    flex-direction: row;
-    -webkit-flex-wrap: nowrap;
-    -ms-flex-wrap: nowrap;
-    flex-wrap: nowrap;
-    -webkit-justify-content: flex-end;
-    -ms-flex-pack: end;
-    justify-content: flex-end;
-    -webkit-align-content: center;
-    -ms-flex-line-pack: center;
-    align-content: center;
-    -webkit-align-items: flex-start;
-    -ms-flex-align: start;
-    align-items: flex-start;
-    }
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: row;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-flex-wrap: nowrap;
+  -ms-flex-wrap: nowrap;
+  flex-wrap: nowrap;
+  -webkit-justify-content: flex-end;
+  -ms-flex-pack: end;
+  justify-content: flex-end;
+  -webkit-align-content: center;
+  -ms-flex-line-pack: center;
+  align-content: center;
+  -webkit-align-items: flex-start;
+  -ms-flex-align: start;
+  align-items: flex-start;
+}
 
 .flex-item {
-    -webkit-order: 0;
-    -ms-flex-order: 0;
-    order: 0;
-    -webkit-flex: 0 1 auto;
-    -ms-flex: 0 1 auto;
-    flex: 0 1 auto;
-    -webkit-align-self: auto;
-    -ms-flex-item-align: auto;
-    align-self: auto;
-    }
+  -webkit-order: 0;
+  -ms-flex-order: 0;
+  order: 0;
+  -webkit-flex: 0 1 auto;
+  -ms-flex: 0 1 auto;
+  flex: 0 1 auto;
+  -webkit-align-self: auto;
+  -ms-flex-item-align: auto;
+  align-self: auto;
+}
 
 .toolbarItem {
   flex: 1;
